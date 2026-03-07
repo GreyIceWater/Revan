ca00cb84c9adc60b8fa704ff41115251f25fce6f
DigitalXcape <kodypweigel@gmail.com>
2026-03-07 10:23:13 -0600

    Fixed with work request + fixed mobile form

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/RegisterController.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/RegisterController.cs
@@ -221,7 +221,7 @@ namespace MidStateShuttleService.Controllers
         /// This is the only Controller action to use view models
         /// </summary>
         /// <returns></returns>
-        [Authorize(Roles = "Admin,Driver")]
+        [Authorize(Roles = "Admin")]
         public IActionResult ViewRegistrations()
         {
             var registrations = new RegisterServices(_context).GetViewModels();
@@ -257,7 +257,7 @@ namespace MidStateShuttleService.Controllers
         }
 
         //displays the details for special request/registrations
-        [Authorize(Roles = "Admin,Driver")]
+        [Authorize(Roles = "Admin")]
         public IActionResult SpecialDetails(int registrationId)
         {
             var model = _context.RegisterModels
